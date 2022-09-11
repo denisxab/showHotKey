1feeed164602624b5a5ee8aa3f2642616460f36a
Your Name <you@example.com>
2022-09-11 15:10:14 +0300

    "Вс 11 сен 2022 15:10:14" -

---
+++ NestedDict.js
@@ -0,0 +1,176 @@
+class NestedDict {
+	/*
+	
+		# Структура для хранения вложенных путей и навигации по ним.
+
+
+		Например вот так будут храниться пути из комбинации клавиш
+        
+        ```js
+		date = {
+			"CTRL_R": {
+					"C": {},
+					"V": {},
+				"SHIFT_L": {
+					"ALT_L": {
+						"F": {}
+				},
+					"C": {},
+					"D": {}
+				}
+			},
+			"F": {}
+		}
+        ```
+
+		Навигация по комбинациям клавишам будет происходить через указания полного пути, или перемещения на уровень в низ по указному ключу.
+		
+		- Например если нужно перейти на уровень в низ для `CTRL_R`, то тогда выполняем 
+
+            ```js
+            NestedDict().nextFomTake('CTRL_R') 
+            ```
+            
+            В итоге в атрибуте `take` будет путь
+            
+            ```js
+            take = {
+                "C": {},
+                "V": {},
+                "SHIFT_L": {
+                    "ALT_L": {
+                        "F": {}
+                    },
+                    "C": {},
+                    "D": {}
+                }
+            },
+            ```
+        - Например если нам нужно перейти на путь `Ctrl_L + Shift_L` не зависимо  от тогда где вы сейчас находись, то тогда выполняем
+            
+            ```js
+            NestedDict().findStartFromDict(['CTRL_R','Shift_L']) 
+            ```
+
+            ```js
+            take = {
+                "ALT_L": {
+                "F": {}
+            },
+            ```
+	*/
+	constructor() {
+		this.dict = {};
+		// Структура для хранения навигации во вложенных комбинациях клавиш
+		this.take = undefined;
+	}
+	clear() {
+		// Отчистить структуру
+		this.dict = {};
+		this.take = undefined;
+	}
+	addFomDict(list) {
+		/*
+			Добавить значение в структуру
+
+			Пример list = ['Ctrl_l','Shift_l','C']
+		*/
+		function _self(_dict, _list, _next_index = 0) {
+			/*
+			Функция для вставки в списка в словарь, кратко говоря элементы списка станут ключами словаря
+
+            ::IN:::::::::::::::::::::::::::::::::::::::::::::::::
+        
+            x = ["ctrl_R", "shift", "g", "ctrl_l", "f"];
+            x2 = ["ctrl_R", "shift", "l"];
+        
+            let ar = {
+                ctrl_l: {
+                    shift: {
+                        l: {},
+                    },
+                    ctrl_l: {
+                        f: {},
+                    },
+                },
+                ctrl_R: {
+                    shift: {
+                    },
+                },
+            };
+        
+            let ar2 = list_to_nested_dict(ar, x);
+            let ar3 = list_to_nested_dict(ar2, x2);
+        
+            ::OUT:::::::::::::::::::::::::::::::::::::::::::::::::
+        
+            {
+                "ctrl_l": {
+                    "shift": {
+                        "l": {}
+                    },
+                    "ctrl_l": {
+                        "f": {}
+                    }
+                },
+                "ctrl_R": {
+                    "shift": {
+                    "g": {
+                        "ctrl_l": {
+                            "f": {}
+                        }
+                    },
+                    "l": {}
+                    }
+                }
+            }
+            
+            */
+			let nv = _dict[_list[_next_index]];
+			// Добавляем элементы в словарь пока в списке есть значения
+			if (nv !== undefined) {
+				_self(nv, _list, _next_index + 1);
+			} else {
+				// Если словарь закончился, а в списке еще есть элементы, то продолжаем добавлять элементы в словарь
+				if (_list.length - 1 >= _next_index) {
+					_dict[_list[_next_index]] = {};
+					nv = _dict[_list[_next_index]];
+					_self(nv, _list, _next_index + 1);
+				}
+			}
+			return _dict;
+		}
+		this.dict = _self(this.dict, list);
+	}
+	findStartFromDict(list) {
+		/*
+			Ищем путь с начала словаря `dict`. Элементы `list` являются ключами словаря `dict`
+
+			Пример list = ['Ctrl_l','Shift_l','C']
+		*/
+		// Передвинутся на уровень вверх
+		function _self(_dict, _list, _next_index = 0) {
+			// Итерируемся по элементам списка `_list` которые являются ключами словаря `_dict`
+			// Останавливаемся на последнем ключе `_dict` который есть в `_list`
+			let nv = _list[_next_index];
+			_next_index += 1;
+			if (nv) {
+				return _self(_dict[nv], _list, _next_index);
+			} else {
+				return _dict;
+			}
+		}
+		this.take = _self(this.dict, list);
+	}
+	nextFomTake(key) {
+		// Передвинутся на уровень в низ
+		this.take = this.take[key];
+	}
+	getFromTake(key) {
+		/* Взять значение из текущего пути */
+		if (this.take === undefined) {
+			this.take = this.dict;
+		}
+		return this.take[key];
+	}
+}
--- main.js
+++ main.js
@@ -1,34 +1,5 @@
-//  Разрешенные клавиши
-const AllowedKey = {
-	Ctrl: "1",
-	c: "2",
-	v: "3",
-};
-
-// Связь клавиши с элементов Html виртуальной клавиатуры, {ИмяСимвола:HtmlЭлемент}
-let MappingKeyFromHtmlKeyboard = {};
-
-// Структура для хранения выбранной конфигурации горячих клавиш, в основном нужна для того чтобы хранить вложенные комбинации клавиш
-let TakeHotKey = {};
-
-class NestedDict {
-	add() {}
-	getLast() {}
-}
-
-// Структура для хранения навигации во вложенных комбинациях клавиш
-let NestedTakeKey = {};
-
-// Список клавиш в навигационной панели
-let StackNavKey = {
-	// Текущий список с клавишами
-	current: [],
-	// Список клавиш которые были удалены
-	last: [],
-};
-
 // Все комбинации клавиш {'ИмяПрограммы':{'МестоВ_Программе':{'КомбинацииКлавиш':'ОписаниеКомбинации'}}}
-let HotKeyDict = {
+let G_HotKeyDict = {
 	// В какой программе
 	vscode: {
 		// В каком месте программы
@@ -49,140 +20,88 @@ let HotKeyDict = {
 			"Ctrl_r + v": "Вставить тест",
 			f: "Поиск",
 			"Ctrl_r + shift_l + alt_l + f": "Поиск во всем проекте",
+			"Ctrl_r + shift_l + Ctrl_l + r": "Поиск во всем проекте",
 			"Ctrl_r + shift_l + c": "Поиск во всем проекте",
 			"Ctrl_r + shift_l + d": "Поиск во всем проекте",
 		},
 	},
 	warno: {
 		игра: {
-			"ctrl_r+g": "Группировать отряды",
+			"ctrl_r + g": "Группировать отряды",
 			F: "Быстрый марш",
 			T: "Огонь",
 			spase: "Вкл/Выкл Пауза",
 		},
 	},
 };
+// -------------------------------------------------------------- //
 
+// Связь клавиши с элементов Html виртуальной клавиатуры, {ИмяСимвола:HtmlЭлемент}
+let G_MappingKeyFromHtmlKeyboard = {};
+
+// Структура для хранения выбранной конфигурации горячих клавиш, в основном нужна для того чтобы хранить вложенные комбинации клавиш
+let G_TakeHotKey = new NestedDict();
+
+// Список клавиш в навигационной панели. Работает на подобие стека
+let G_StackNavKey = {
+	// Текущий список с клавишами
+	current: [],
+	// Список клавиш которые были удалены
+	last: [],
+};
 // -------------------------------------------------------------- //
 class Utils {
-	// Класс с универсальными утилитами
+	// ++++++++++++++++++++++
+	/* Класс с универсальными утилитами */
 	static removeOptions(selectElement, end = 1) {
-		// Отчистить `selected` от `options`, по умолчанию оставляем самый первый элемент
-		var i,
+		/* Отчистить `selected` от `options`, по умолчанию оставляем самый первый элемент */
+		let i,
 			L = selectElement.options.length - 1;
 		for (i = L; i >= end; i--) {
 			selectElement.remove(i);
 		}
 	}
-	static add_nested_dict(_dict, _list, _next_index = 0) {
-		// Функция для вставки в списка в словарь, кратко говоря элементы списка станут ключами словаря
-		/*
-        ::IN:::::::::::::::::::::::::::::::::::::::::::::::::
-    
-        x = ["ctrl_R", "shift", "g", "ctrl_l", "f"];
-        x2 = ["ctrl_R", "shift", "l"];
-    
-        let ar = {
-            ctrl_l: {
-                shift: {
-                    l: {},
-                },
-                ctrl_l: {
-                    f: {},
-                },
-            },
-            ctrl_R: {
-                shift: {
-                },
-            },
-        };
-    
-        let ar2 = list_to_nested_dict(ar, x);
-        let ar3 = list_to_nested_dict(ar2, x2);
-    
-        ::OUT:::::::::::::::::::::::::::::::::::::::::::::::::
-    
-        {
-            "ctrl_l": {
-                "shift": {
-                    "l": {}
-                },
-                "ctrl_l": {
-                    "f": {}
-                }
-            },
-            "ctrl_R": {
-                "shift": {
-                "g": {
-                    "ctrl_l": {
-                        "f": {}
-                    }
-                },
-                "l": {}
-                }
-            }
-        }
-        
-        */
-		let nv = _dict[_list[_next_index]];
-		// Добавляем элементы в словарь пока в списке есть значения
-		if (nv !== undefined) {
-			this.add_nested_dict(nv, _list, _next_index + 1);
-		} else {
-			// Если словарь закончился, а в списке еще есть элементы, то продолжаем добавлять элементы в словарь
-			if (_list.length - 1 >= _next_index) {
-				_dict[_list[_next_index]] = {};
-				nv = _dict[_list[_next_index]];
-				this.add_nested_dict(nv, _list, _next_index + 1);
-			}
+	static getFromSelect(html_id) {
+		/* Получить `индекс,текст` из `select` по указному `id` */
+		let elm = document.getElementById(html_id);
+		if (elm.selectedIndex <= 0) {
+			// Исключаем значение по умолчанию
+			return null;
 		}
-		return _dict;
+		return [elm.selectedOptions[0].index, elm.selectedOptions[0].text];
 	}
 }
 
 class UserSelect {
-	static SelectProgram() {
-		// Получить `индекс,текст` выбранной программы
-		let elm = document.getElementById("select-program");
-		if (elm.selectedIndex <= 0) {
-			// Исключаем значение по умолчанию
-			return null;
-		}
-		return [elm.selectedOptions[0].index, elm.selectedOptions[0].text];
-	}
-	static SelectPlace() {
-		// Получить `индекс,текст` выбранного места
-		let elm = document.getElementById("select-place");
-		if (elm.selectedIndex <= 0) {
-			// Исключаем значение по умолчанию
-			return null;
-		}
-		return [elm.selectedOptions[0].index, elm.selectedOptions[0].text];
-	}
+	// ++++++++++++++++++++++
 	static BuildSelectProgram() {
-		// Заполнить список программ на основе HotKeyDict
-		let start = 0;
-		let end = Object.keys(HotKeyDict).length - 1;
+		/* Заполнить список программ на основе HotKeyDict */
+		Utils.removeOptions(document.getElementById("select-program"));
+		let end = Object.keys(G_HotKeyDict).length - 1;
 		let elm_program = document.getElementById("select-program");
-		for (let i = start; i <= end; i++) {
+		for (let i = 0; i <= end; i++) {
 			let opt = document.createElement("option");
-			opt.innerHTML = Object.keys(HotKeyDict)[i];
+			opt.innerHTML = Object.keys(G_HotKeyDict)[i];
 			elm_program.appendChild(opt);
 		}
 	}
 	static BuildSelectPlace() {
-		// Заполнить список программ на основе HotKeyDict и выбранной программы
-		console.log("BuildSelectPlace");
+		/* Заполнить список программ из HotKeyDict на основе выбранной программы */
+		// Отчищаем данные из навигационной панели
+		NavKey.ClearNavKey();
+		// Отчищаем список от прошлых горячих клавиш
+		ListHotKey.ClearList();
+		// Удаляем выбор со всех клавиш виртуальной клавиатуры
+		VirtualHotKey.ClearTakeKey();
 		// Отчистить прошлые значения от предыдущей программы
 		Utils.removeOptions(document.getElementById("select-place"));
 		// Выбранная программа
-		let select_program = UserSelect.SelectProgram()[1];
-		let start = 0;
-		let end = Object.keys(HotKeyDict[select_program]).length - 1;
+		let select_program = Utils.getFromSelect("select-program")[1];
+		let end = Object.keys(G_HotKeyDict[select_program]).length - 1;
 		let elm_place = document.getElementById("select-place");
-		for (let i = start; i <= end; i++) {
+		for (let i = 0; i <= end; i++) {
 			let opt = document.createElement("option");
-			opt.innerHTML = Object.keys(HotKeyDict[select_program])[i];
+			opt.innerHTML = Object.keys(G_HotKeyDict[select_program])[i];
 			elm_place.appendChild(opt);
 		}
 	}
@@ -205,21 +124,29 @@ class UserSelect {
 		this.BuildSelectProgram();
 		// Добавляем обработчик событий на выбор программы
 		this._addEventProgramSelect();
+		// Добавляем обработчик событий на выбор места
 		this._addEventPlaceSelect();
 	}
 }
 
 class NavKey {
+	static ClearNavKey() {
+		G_StackNavKey.last = [];
+		G_StackNavKey.current = [];
+		NavKey.ShowNavKey();
+	}
 	static ShowNavKey() {
+		/* Показать клавиши в навигационной панели */
 		const elm = document.getElementById("nav_keboard_date");
-		elm.innerText = StackNavKey.current.join("+");
+		elm.innerText = G_StackNavKey.current.join("+");
 	}
 	static addNavKey(val) {
 		// Добавить клавишу в навигационную панель
 		console.log("addNavKey");
 		// Добавляем клавишу в список выбранных, если она еще не добавлена в конец
-		if (StackNavKey.current[StackNavKey.current.length - 1] !== val) {
-			StackNavKey.current.push(val);
+		if (G_StackNavKey.current[G_StackNavKey.current.length - 1] !== val) {
+			G_StackNavKey.current.push(val);
+			G_StackNavKey.last.pop();
 			NavKey.ShowNavKey();
 		}
 	}
@@ -227,35 +154,29 @@ class NavKey {
 		// Удалить с конца клавишу в навигационной панели
 		console.log("popNavKey");
 		// Если в текущей навигации есть клавиша
-		if (StackNavKey.current[0] !== undefined) {
+		if (G_StackNavKey.current[0] !== undefined) {
 			// Удаляем клавишу из текущей навигации, и заносим ей в предыдущие
-			StackNavKey.last.push(StackNavKey.current.pop());
-			function get_last_key_nested_dict(_dict, _list, _next_index = 0) {
-				// Итерируемся по элементам списка `_list` которые являются ключами словаря `_dict`
-				// Останавливаемся на последнем ключе `_dict` который есть в `_list`
-				let nv = _list[_next_index];
-				_next_index += 1;
-				if (nv) {
-					return get_last_key_nested_dict(_dict[nv], _list, _next_index);
-				} else {
-					return _dict;
-				}
-			}
+			G_StackNavKey.last.push(G_StackNavKey.current.pop());
 			// Берем предыдущею комбинацию клавиш
-			NestedTakeKey = get_last_key_nested_dict(TakeHotKey, StackNavKey.current);
+			G_TakeHotKey.findStartFromDict(G_StackNavKey.current);
 			// Показываем доступные клавиши, для предыдущей комбинации
-			VirtualHotKey._ShowClickKey(NestedTakeKey);
+			VirtualHotKey.ShowClickKey(G_TakeHotKey.take);
 			// Показываем текущий путь в навигационную панель клавиш
 			NavKey.ShowNavKey();
-			//------------------------
 		}
 	}
 	static backNavKey() {
 		// Вернуть ранее удаленную клавишу
 		// Проверяем на то что есть клавиша которую можно возвращать
 		console.log("backNavKey");
-		if (StackNavKey.last[0] !== undefined) {
-			StackNavKey.current.push(StackNavKey.last.pop());
+		if (G_StackNavKey.last[0] !== undefined) {
+			// Удаляем клавишу из предыдущих, и заносим ей в текущие
+			G_StackNavKey.current.push(G_StackNavKey.last.pop());
+			// Берем комбинацию клавиш которая была возвращена из стека
+			G_TakeHotKey.findStartFromDict(G_StackNavKey.current);
+			// Показываем доступные клавиши, для текущей комбинации
+			VirtualHotKey.ShowClickKey(G_TakeHotKey.take);
+			// Показываем текущий путь в навигационную панель клавиш
 			NavKey.ShowNavKey();
 		}
 	}
@@ -284,19 +205,19 @@ class VirtualHotKey {
 			
 			list_hot_key: HotKeyDict[select_program][select_place]
 		*/
+		// Отчищаем данные из навигационной панели
+		NavKey.ClearNavKey();
 		// Отчищаем предыдущие занятые клавиши
 		VirtualHotKey.ClearTakeKey();
 		// Отчищаем словарь занятые комбинации от прошлых
-		TakeHotKey = {};
+		G_TakeHotKey.clear();
 		for (let x of Object.keys(list_hot_key)) {
 			// Переводим комбинацию клавиш в стандартный вид
 			x = ParseHotKey._BaseParse(x);
 			// Заносим комбинацию в структуру в вложенный словарь, для дальнейше логики вложенных комбинации клавиш
-			TakeHotKey = Utils.add_nested_dict(TakeHotKey, x);
-			// Делаем глубокую копию. Это нужно для навигации во вложенных комбинациях клавиш
-			NestedTakeKey = JSON.parse(JSON.stringify(TakeHotKey));
+			G_TakeHotKey.addFomDict(x);
 			// Выделяем первые клавиши из комбинации
-			let elm = MappingKeyFromHtmlKeyboard[x[0]];
+			let elm = G_MappingKeyFromHtmlKeyboard[x[0]];
 			console.log(elm);
 			// Условие для простых комбинаций клавиш, состоящие из одного символа
 			if (x.length == 1) {
@@ -309,13 +230,13 @@ class VirtualHotKey {
 		}
 	}
 
-	static _ShowClickKey(list_key) {
+	static ShowClickKey(list_key) {
 		// Отчищаем занятые комбинации, будем показывать те комбинации которые доступные через клавишу `elm_value`
 		VirtualHotKey.ClearTakeKey();
 		for (let x in list_key) {
 			console.log(x);
 			console.log(list_key[x]);
-			let elm_key = MappingKeyFromHtmlKeyboard[x];
+			let elm_key = G_MappingKeyFromHtmlKeyboard[x];
 			if (Object.keys(list_key[x]).length > 0) {
 				// Если это вложенная комбинация клавиш
 				elm_key.parentElement.classList.add("take-nested-key");
@@ -339,12 +260,12 @@ class VirtualHotKey {
 		// Добавляем эту клавишу в навигационную панель
 		NavKey.addNavKey(elm_value);
 		// Показываем доступные клавиши
-		VirtualHotKey._ShowClickKey(
+		VirtualHotKey.ShowClickKey(
 			// Получаем все доступные комбинации клавиши(на одном уровне) для текущей нажатой клавиши.
-			NestedTakeKey[elm_value]
+			G_TakeHotKey.getFromTake(elm_value)
 		);
 		// Переходи на уровень в низ, на те комбинации которые доступны через нажатую клавишу
-		NestedTakeKey = NestedTakeKey[elm_value];
+		G_TakeHotKey.nextFomTake(elm_value);
 	}
 
 	static _ClickKey(event) {
@@ -373,7 +294,7 @@ class VirtualHotKey {
 		// Заполняем MappingKeyFromHtmlKeyboard значениями из виртуальной клавиатуры
 		VirtualHotKey.GetAllKey().forEach((elm) => {
 			console.log(`${elm.value}: ${elm}`);
-			MappingKeyFromHtmlKeyboard[elm.value.toUpperCase()] = elm;
+			G_MappingKeyFromHtmlKeyboard[elm.value.toUpperCase()] = elm;
 		});
 	}
 	static init() {
@@ -400,7 +321,7 @@ class ParseHotKey {
 
 class ListHotKey {
 	// Список с комбинациями клавиш, в зависимости от выбранной программы и места использования
-	static _ClearList() {
+	static ClearList() {
 		// Отчистить список с комбинациями клавиш
 		let elm = document.getElementById("list_hot_key");
 		elm.innerHTML = "";
@@ -410,17 +331,17 @@ class ListHotKey {
 		// Показать список горячих клавиш для выбранной программы и места
 		console.log("AgainShowListHotKey");
 		// Отчищаем список от прошлых горячих клавиш
-		ListHotKey._ClearList();
+		ListHotKey.ClearList();
 		// Выбранная программа
-		let select_program = UserSelect.SelectProgram();
-		let select_place = UserSelect.SelectPlace();
+		let select_program = Utils.getFromSelect("select-program");
+		let select_place = Utils.getFromSelect("select-place");
 		if ((select_program !== null) & (select_place !== null)) {
 			// Если не null то берем нормальные значения
 			select_program = select_program[1];
 			select_place = select_place[1];
 			let start = 0;
 			// Список горячих клавиш
-			let list_hot_key = HotKeyDict[select_program][select_place];
+			let list_hot_key = G_HotKeyDict[select_program][select_place];
 			let end = Object.keys(list_hot_key).length - 1;
 			// Создаем элемент для хранения комбинации клавиш
 			let elm_place = document.getElementById("list_hot_key");
